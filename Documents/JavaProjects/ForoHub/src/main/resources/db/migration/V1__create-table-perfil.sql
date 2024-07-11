2b93a5ef27319ff6001d0215154a70e67c12075c
navasan <inaki_nava@hotial.com>
2024-07-11 16:52:03 -0600

    Modelo

--- Documents/JavaProjects/ForoHub/src/main/resources/db/migration/V1__create-table-perfil.sql
+++
@@ -1,6 +0,0 @@
-create table perfil(
-     id bigint not null auto_increment,
-     nombre varchar(100) not null,
-
-     primary key(id)
-);
---
+++ src/main/java/com/navasan/ForoHub/domain/curso/Curso.java
@@ -0,0 +1,19 @@
+package com.navasan.ForoHub.domain.curso;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Curso")
+@Table(name = "curso")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
